dfe47f5b3c308263ed58e6c27b32e0545b7044ae
Abdul Rahman Azam <69141440+AbdulRahmanAzam@users.noreply.github.com>
2025-12-12 16:56:55 +0500

    Update API_URL to use HTTPS

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 
-const API_URL = 'http://jobguardapi.abdulrahmanazam.me/api'
+const API_URL = 'https://jobguardapi.abdulrahmanazam.me/api'
 // const API_URL = 'http://localhost:8000/api'
 
 function App() {

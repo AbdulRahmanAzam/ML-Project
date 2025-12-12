96efd6f0555ab69cee6c8807e788ac4510da7594
Abdul Rahman Azam <69141440+AbdulRahmanAzam@users.noreply.github.com>
2025-12-12 16:55:53 +0500

    Switch API_URL to production endpoint

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,8 @@
 import { useState } from 'react'
 import './App.css'
 
-// const API_URL = 'http://jobguardapi.abdulrahmanazam.me/api'
-const API_URL = 'http://localhost:8000/api'
+const API_URL = 'http://jobguardapi.abdulrahmanazam.me/api'
+// const API_URL = 'http://localhost:8000/api'
 
 function App() {
   const [formData, setFormData] = useState({
